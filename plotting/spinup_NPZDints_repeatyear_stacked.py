94bd8715bc993239e9c96aa0bbba3fdfc9ed0e74
Tia Bottger <tiabottger@gmail.com>
2026-07-30 15:03:22 -0700

    edit spinup_NPZDints_repeatyear_stacked.py

--- plotting/spinup_NPZDints_repeatyear_stacked.py
+++ plotting/spinup_NPZDints_repeatyear_stacked.py
@@ -304,7 +304,7 @@ for region in regions:
         )
 
         # Choose a colormap for the reruns
-        colors = plt.cm.Dark2(np.linspace(0, 1, len(gtagexes)))
+        colors = plt.cm.Reds(np.linspace(0, 1, len(gtagexes)))
 
         for g, gtagex in enumerate(gtagexes):
 
