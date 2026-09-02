1fd6d1d4777280a00d5702249fad6e5fec889bd1
Tia Bottger <tiabottger@gmail.com>
2026-09-02 16:54:04 -0700

    fix km3 conversion in hypoxic volume bottom_DO_plots.py

--- intermodel_comparison/bottom_DO/bottom_DO_plots.py
+++ intermodel_comparison/bottom_DO/bottom_DO_plots.py
@@ -186,7 +186,7 @@ hyp_volume_LO_ts = np.nansum(
     hyp_thick_LO *
     DA_ps[None, :, :],
     axis=(1, 2) 
-)/10e3
+)/1e3
 
 # ============================================================
 # SALISHSEACAST
@@ -221,7 +221,7 @@ hyp_volume_SSC_ts = np.nansum(
     hyp_thick_SSC *
     DA_SSC[None, :, :],
     axis=(1, 2)
-)/ 10e3 # convert m hyp thick to km --> km3
+)/ 1e3 # convert m hyp thick to km --> km3
        
 # ============================================================
 # PLOT HYPOXIC AREA AND VOLUME TS
